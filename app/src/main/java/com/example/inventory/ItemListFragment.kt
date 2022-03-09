71fe5e096c8f686a52f7aadfa9a86036fdcfc16c
ThomasGiros <thomas.giros@gmail.com>
2022-03-09 11:33:11 -0500

    Layout set to attach parent false and added linear segment to make the list view positionning coherent

--- app/src/main/java/com/example/inventory/ItemListFragment.kt
+++ app/src/main/java/com/example/inventory/ItemListFragment.kt
@@ -20,9 +20,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.inventory.adapter.ItemListAdapter
 import com.example.inventory.databinding.ItemListFragmentBinding
 
 /**
@@ -33,18 +36,36 @@ class ItemListFragment : Fragment() {
     private var _binding: ItemListFragmentBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database.itemDao()
+        )
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = ItemListFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ItemListAdapter {
+        }
+        binding.recyclerView.adapter = adapter
+
+        viewModel.allItems.observe(this.viewLifecycleOwner) { items ->
+            items.let {
+                adapter.submitList(it)
+            }
+        }
         binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
+
         binding.floatingActionButton.setOnClickListener {
             val action = ItemListFragmentDirections.actionItemListFragmentToAddItemFragment(
                 getString(R.string.add_fragment_title)
